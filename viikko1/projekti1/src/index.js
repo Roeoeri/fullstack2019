1338dedb16a0b67dc1626ab8791318a98a2342f4
tomih <tomi.haapakoski@helsinki.fi>
2019-01-25 16:29:50 +0200

    Tehtävät 1.1-1.2 tehty

--- viikko1/projekti1/src/index.js
+++ viikko1/projekti1/src/index.js
@@ -1,6 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) =>{
+    return(
+     <h1>{props.kurssi}</h1>
+    )
+}
+
+const Part = (props) =>{
+    return(
+        <div>
+            <p>
+        {props.sisalto}  {props.maara}
+            </p>
+        </div>
+    )
+}
+
+const Total = (props)=>{
+    return(
+        <div>
+            <p>
+                yhteensä {props.yhteensa} tehtävää
+            </p>
+        </div>
+    )
+}
+
+const Content= (props) =>{
+    return(
+        <div>
+            <Part sisalto ={props.sisalto1} maara={props.maara1}/>
+            <Part sisalto ={props.sisalto2} maara={props.maara2}/>
+            <Part sisalto ={props.sisalto3} maara={props.maara3}/>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
   const part1 = 'Reactin perusteet'
@@ -12,17 +55,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
+      <Header kurssi={course}/>
+
+      
+      <Content sisalto1={part1} maara1={exercises1}
+               sisalto2={part2} maara2={exercises2}
+               sisalto3={part3} maara3={exercises3}/>
+
+
+      <Total yhteensa={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
