5cf6e6c94b4f8476c5dc9502e780874b36fc39fa
tomih <tomi.haapakoski@helsinki.fi>
2019-02-04 02:18:01 +0200

    puhelinluettelo valmis

--- viikko2/projekti2_puhelinluettelo/src/App.js
+++ viikko2/projekti2_puhelinluettelo/src/App.js
@@ -1,12 +1,41 @@
 
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+
+
+
+const PassMessage = ({message}) =>{
+  if(message === null){
+    return null
+  }
+  return(
+    <div className="pass">
+    {message}
+    </div>
+  )
+
+}
+
+const ErrorMessage = ({message}) =>{
+  if(message === null){
+    return null
+  }
+  return(
+    <div className="error">
+    {message}
+    </div>
+  )
+  
+}
 
 
 const Person = (props)=>{
   return(
    <div>
      {props.person.name} {props.person.number}
+     <PoistoNappi poista = {props.poista}/>
    </div>
   )
 
@@ -19,8 +48,16 @@ const AllPerson = (props)=>{
 
   return(
     personsToshow.map(person =>
-    <Person key={person.name} person = {person}/>)
+    <Person poista={()=> {props.poista(person.id)}} key={person.id} person = {person}/>)
+    
   )
+}
+
+const PoistoNappi = (props) =>{
+  return(
+  <button onClick={props.poista}> Poista </button>
+  )
+
 }
 
 const FilterLomake = (props) =>{
@@ -49,7 +86,7 @@ const NimiNumeroLomake = (props) =>{
     onChange= {props.onChangeNumber}
     />
     </div>
-    <button type = {props.buttonType}>{}props.buttonName</button>
+    <button type = {props.buttonType}>{props.buttonName}</button>
     </form>
   )
 
@@ -58,10 +95,23 @@ const NimiNumeroLomake = (props) =>{
 
 const App = () => {
 
-  const [ persons, setPersons] = useState([]) 
+  const [ persons, setPersons] = useState([
+    
+  ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] =useState("")
   const [ newFilter, setNewFilter] =useState("")
+  const [ passMessage, setPassMessage] = useState(null)
+  const [ errorMessage, setErrorMessage] = useState(null)
+
+
+  useEffect(()=> {
+    personService
+    .getAll()
+    .then(response => {
+      setPersons(response.data)
+    })
+  },[])
   
   const handleNameChange = (event) =>{
     setNewName(event.target.value)
@@ -80,28 +130,120 @@ const App = () => {
 
   const addNimiNumero = (event) =>{
     event.preventDefault()
+
     const nameNumberObject  ={
     name: newName,
-    number: newNumber
+    number: newNumber,
+    
     }
     for(var i=0; i<persons.length;i++){
       if(persons[i].name===newName){
+        if(
+        window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella ?`)){
+
+          const id = persons[i].id
+          const number = nameNumberObject.number
+          personUpdate(id,number)
+        
+        }
         setNewName("")
         setNewNumber("")
-        window.alert(`${newName} on jo luettelossa`)
         return
       }
     }
+
+    personService
+    .add(nameNumberObject)
+    .then(response=>{
+      setPersons(persons.concat(response.data))
+      setNewName("")
+      setNewNumber("")
+      setPassMessage(
+        "Henkilö " + nameNumberObject.name + " lisättiin"
+      )
+      setTimeout(()=>{
+        setPassMessage(null)
+      },5000)
     
-    setPersons(persons.concat(nameNumberObject))
-    setNewName("")
-    setNewNumber("")
+
+
+    })
+
+    
   }
 
+  const personUpdate = (id, number) =>{
+    const person = persons.find(p => p.id === id)
+    const updatedPerson = {...person, number: number}
+
+    personService
+    .update(id, updatedPerson)
+    .then(response =>{
+      setPersons(persons.map(person => person.id !== id ? person: response.data))
+      setPassMessage(
+        "Henkilön " + getPersonName(id) + " tietoja muutettin"
+      )
+      setTimeout(()=>{
+        setPassMessage(null)
+      },5000)
+    })
+    .catch(error =>{
+      setErrorMessage(
+        "Henkilö " + getPersonName(id) + " on jo poistettu"
+      )
+      setPersons(persons.filter(person=> person.id !== id))
+      setTimeout(()=>{
+        setErrorMessage(null)
+      },5000)
+
+    })
+
+  }
+
+  
+  const personDelete = (id) =>{
+
+    if(window.confirm("Poistetaanko " + getPersonName(id) + " ?")){
+    personService
+    .deletePerson(id)
+    .then(response =>{
+      setPersons(persons.filter(person=> person.id !== id))
+      setPassMessage(
+        "Henkilö " + getPersonName(id) + " poistettiin"
+      )
+      setTimeout(()=>{
+        setPassMessage(null)
+      },5000)
+    }).catch(error =>{
+      setErrorMessage(
+        "Henkilö " + getPersonName(id) + " on jo poistettu"
+      )
+      setPersons(persons.filter(person=> person.id !== id))
+      setTimeout(()=>{
+        setErrorMessage(null)
+      },5000)
+    })
+    
+    }
+  }
+
+  const getPersonName = (id) =>{
+    let person
+    for(let i=0; i<persons.length;i++){
+        if(persons[i].id === id){
+           person=persons[i].name
+        }
+    }
+    return(
+      person
+    )
+  }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <PassMessage message = {passMessage} />
+      <ErrorMessage message = {errorMessage} />
      Hae nimistä: 
      <FilterLomake value= {newFilter} handler={handleFilterChange}/>
       <h2>Lisää uusi</h2>
@@ -110,7 +252,7 @@ const App = () => {
      value2= {newNumber} onChangeNumber={handleNumberChange} buttonType="submit"
      buttonName = "lisää" /> 
       <h2>Numerot</h2>
-      <AllPerson allPersons={persons} newFilter={newFilter}/>
+      <AllPerson poista={personDelete} allPersons={persons} newFilter={newFilter}/>
     </div>
 
   )
---
+++ viikko2/projekti2_puhelinluettelo/src/services/persons.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons/'
+
+
+const getAll = () =>{
+
+    return axios.get(baseUrl)
+
+}
+
+const add = newObject =>{
+    return axios.post(baseUrl, newObject)
+}
+
+const update = (id, newObject) =>{
+    return axios.put(baseUrl + id, newObject)
+    
+}
+
+const deletePerson = (id) =>{
+      return axios.delete(baseUrl + id)
+}
+
+
+export default {
+    getAll: getAll,
+    add: add,
+    update: update,
+    deletePerson: deletePerson
+}
+
